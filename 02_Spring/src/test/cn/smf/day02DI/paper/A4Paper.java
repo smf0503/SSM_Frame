0b3d0b238810aae3286499de081130760631c2d9
smf0503 <1617756812@qq.com>
2017-10-10 14:52:15 +0800

    A4Paper第一次

---
+++ 02_Spring/src/test/cn/smf/day02DI/paper/A4Paper.java
@@ -0,0 +1,10 @@
+package cn.smf.day02DI.paper;
+
+/**
+ * Created by ASUS on 2017/10/10.
+ */
+public class A4Paper implements Paper {
+    public String getPaper() {
+        return "A4";
+    }
+}
